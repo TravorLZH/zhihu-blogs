88efec9a1c211e255b3c29a8ebb7ec2ca9ec615e
Travor Liu <travor_lzh@outlook.com>
2022-02-18 10:23:37 +0800

    Update cover for 1+3

--- goldbach/goldbach9.py
+++ goldbach/goldbach9.py
@@ -7,12 +7,14 @@ class FooScene(Scene):
             r"^{\frac13}":RED,
             r"^{1\over10}":RED,r"\frac1{10}":RED,
             "w":PINK,"_j":YELLOW,"{x}":GREEN,
-            "{P}":BLUE
+            "{P}":BLUE,
+            r"\prod":BLUE
         }
         tit=TextMobject("Goldbach's Problem \#9")
         eqn=TexMobject(r"\#\{p\le x:x-p=P_3\}\ge{P}_w({x},1,{x}^{1\over10})",
                 tex_to_color_map=mp)
         eqn2=TexMobject(r"-\frac12\sum_{{x}^{1\over10}<p_j\le{x}^{\frac13}}{P}_{w_j}({x},p_j,{x}^{1\over10})+O({x}^{1-\frac1{10}})",
                 tex_to_color_map=mp)
-        self.add(VGroup(tit,eqn,eqn2).arrange(DOWN))
+        eqn3=TexMobject(r"\ge2.0586\prod_{\substack{p|x\\p>2}}{p-1\over p-2}\prod_{p>2}\left(1-{1\over(p-1)^2}\right){{x}\over\log^2{x}}",tex_to_color_map=mp)
+        self.add(VGroup(tit,eqn,eqn2,eqn3).arrange(DOWN))
         self.wait()
